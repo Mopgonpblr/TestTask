8c2b1394c92c8f454b7e9adb7b76eb5ab96a9799
Mopgonpblr <65500580+Mopgonpblr@users.noreply.github.com>
2024-02-12 20:35:40 +0300

    Add files via upload

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+
+    public class OrderService : IOrderService
+    {
+        public Task<Order> GetOrder()
+        {
+            return Task.Run(() =>
+            {
+                var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test")
+        .Options;
+                using ApplicationDbContext db = new(contextOptions);
+                Order max = db.Orders.First();
+                foreach (Order order in db.Orders)
+                {
+                    if (order.Price > max.Price)
+                        max = order;
+                }
+                return max;
+            });
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return Task.Run(() =>
+            {
+
+                var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test").Options;
+                using ApplicationDbContext db = new(contextOptions);
+                var orders = from order in db.Orders
+                             where order.Quantity > 10
+                             select order;
+
+                return orders.ToList<Order>();
+            });
+        }
+    }
+
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using TestTask.Controllers;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+
+        public Task<User> GetUser()
+        {
+            return Task.Run(() =>
+            {
+                var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test")
+        .Options;
+                using ApplicationDbContext db = new(contextOptions);
+                User max = db.Users.First();
+                foreach (User user in db.Users)
+                {
+                    if (user.Orders != null && user.Orders.Count > max.Orders.Count)
+                        max = user;
+                }
+                return max;
+            });
+        }
+
+
+        public Task<List<User>> GetUsers()
+        {
+            return Task.Run(() =>
+            {
+                var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test").Options;
+                using ApplicationDbContext db = new(contextOptions);
+                var users = from user in db.Users
+                            where user.Status == UserStatus.Inactive
+                            select user;
+
+                return users.ToList<User>();
+            });
+        }
+    }
+}
